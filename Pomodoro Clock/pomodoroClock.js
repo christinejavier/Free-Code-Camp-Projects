b191cbef389a3798464988d86b40589756a78937
christinejavier <cjavier13@ucsbalum.com>
2016-05-01 20:23:46 -0700

    Pauses and resumes activity countdown

--- Pomodoro Clock/pomodoroClock.js
+++ Pomodoro Clock/pomodoroClock.js
@@ -1,72 +1,92 @@
 $( document ).ready(function() {
+  $("#pressPause").hide();
+  $("#pressResume").hide();
+
   var activityTimeVal; //Amount of time to do activity
-  var originalVal1 = activityTimeVal;
   var breakTimeVal; //Amount of break time
+  var checkMins = $("#activityMins").text(); //Placeholder for user's desired activity time
+  var seconds = 59; //Seconds used for countdown
+  var countingDown;
+  var stopCountingDown;
 
-  $("#addActivityTime").click(addActivityTime); //Click event for + anchor of main activity time
-  $("#minusActivityTime").click(minusActivityTime); // Click event for - anchor of main activity time
+  $("#addActivityTime").click(addActivityTime);
+  $("#minusActivityTime").click(minusActivityTime);
+  $("#addBreakTime").click(addBreakTime);
+  $("#minusBreakTime").click(minusBreakTime);
+  $("#pressStart").click(timerSeconds);
+  $("#pressPause").click(pauseActivity);
+  $("#pressResume").click(resumeActivity);
 
-  $("#addBreakTime").click(addBreakTime); //Click event for + anchor of break time
-  $("#minusBreakTime").click(minusBreakTime); // Click event for - anchor of break time
-
-  $("#pressStart").click(timerSeconds); //Starts Pomodoro Clock
 
   function addActivityTime() { //Adds one minute to activity time
-    activityTimeVal = $("#activityTime").text(); //Refers to 25 min in h1 tag
+    activityTimeVal = $("#activityMins").text(); //Refers to 25 min in h1 tag
     activityTimeVal = parseInt(activityTimeVal); //Changes 25 from string to number
     activityTimeVal = addOne(activityTimeVal); //Callback function to add one minute
-    var showThis = activityTimeVal + ":" + "00";
-    $("#activityTime").text(showThis); //Displays activity time
+    $("#activityMins").text(activityTimeVal); //Displays activity time
+    checkMins = $("#activityMins").text(); //Sets checkMins to user's ideal time. Allows for accurate countdown
   }
 
   function minusActivityTime() { //Subtracts one minute from activity time
-    activityTimeVal = $("#activityTime").text();//Refers to 25 min in h1 tag
+    activityTimeVal = $("#activityMins").text();//Refers to 25 min in h1 tag
     activityTimeVal = parseInt(activityTimeVal);//Changes 25 from string to number
     if(activityTimeVal === 5) { //Limits minimum activity time to 5 minutes
-      showThis = activityTimeVal + ":" + "00"
-      return $("#activityTime").text(showThis);
+      return $("#activityMins").text(activityTimeVal);
     }
     activityTimeVal = subtractOne(activityTimeVal); //Callback function to subtract one minute
-    var showThis = activityTimeVal + ":" + "00";
-    $("#activityTime").text(showThis); //Displays activity time
+    $("#activityMins").text(activityTimeVal); //Displays activity time
+    checkMins = $("#activityMins").text(); //Sets checkMins to user's ideal time. Allows for accurate countdown
   }
 
   function addBreakTime() { //Adds one minute to activity time
-    breakTimeVal = $("#breakTime").text(); //Refers to 5 min in h1 tag
+    breakTimeVal = $("#breakMins").text(); //Refers to 5 min in h1 tag
     breakTimeVal = parseInt(breakTimeVal); //Changes 5 from string to number
     if(breakTimeVal === 30) { // Limits max break time to 30 minutes
-      showThis = breakTimeVal + ":" + "00"
-      return $("#breakTime").text(showThis);
+      return $("#breakMins").text(breakTimeVal);
     }
     breakTimeVal = addOne(breakTimeVal); //Callback function to add one minute
-    var showThis = breakTimeVal + ":" + "00";
-    $("#breakTime").text(showThis); //Displays break time
+    $("#breakMins").text(breakTimeVal); //Displays break time
   }
 
   function minusBreakTime() { //Subtracts one minute from activity time
-    breakTimeVal = $("#breakTime").text();//Refers to 5 min in h1 tag
+    breakTimeVal = $("#breakMins").text();//Refers to 5 min in h1 tag
     breakTimeVal = parseInt(breakTimeVal);//Changes 5 from string to number
     if(breakTimeVal === 1) { //Limits minimum break time to 1 minute
-      showThis = breakTimeVal + ":" + "00"
-      return $("#breakTime").text(showThis);
+      return $("#breakMins").text(breakTimeVal);
     }
     breakTimeVal = subtractOne(breakTimeVal); //Callback function to subtract one minute
-    var showThis = breakTimeVal + ":" + "00";
-    $("#breakTime").text(showThis); //Displays break time
+    $("#breakMins").text(breakTimeVal); //Displays break time
   }
 
 
-  function timerSeconds() {
-    setInterval(countdownActivitySeconds, 1000);
+  function timerSeconds() { //Initiates timer
+    $("#pressStart").hide();
+    $("#pressPause").show();
+    countingDown = setInterval(countdownActivitySeconds, 1000);
+    countingDown();
   }
 
-  var seconds = 59;
-  var checkMins = $("#activityMins").text();
-  function countdownActivitySeconds() {
+  function pauseActivity() { //Pauses timer
+    $("#pressPause").hide();
+    $("#pressResume").show();
+    stopCountingDown = clearInterval(countingDown);
+    stopCountingDown();
+  }
+
+  function resumeActivity() { //Resumes timer
+    $("#pressPause").show();
+    $("#pressResume").hide();
+    countingDown = setInterval(countdownActivitySeconds, 1000);
+    countingDown();
+  }
+
+  function countdownActivitySeconds() { //Counts down seconds of activity timer
     if($("#activityMins").text() === checkMins) {
       countdownActivityMins();
     }
     if (seconds < 0) {
+      if($("#activityMins").text() === $("#activityMins").text(0)) { //Stops countdown once time is all zeroes
+      //CONTINUE HERE
+      }
       countdownActivityMins();
       seconds = 59;
       $("#activitySecs").text(seconds);
@@ -81,11 +101,14 @@ $( document ).ready(function() {
     seconds--;
   }
 
-  function countdownActivityMins() {
+  function countdownActivityMins() { //Counts down minutes of activity timer
     var mins = $("#activityMins").text();
     mins--;
     return $("#activityMins").text(mins);
   }
+
+//CREATE PING FOR WHEN ACTIVITY TIMER IS DONE
+//INITIATE START OF BREAKTIME
 
   function addOne(answer) { //Adds one minute
     answer += 1;
