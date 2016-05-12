55a10f21fd65c57e54e558cf5822d94912c489cd
christinejavier <cjavier13@ucsbalum.com>
2016-05-12 15:34:24 -0700

    Styling of initial set of elements complete

---
+++ Random Quote Machine/quoteMachine.js
@@ -0,0 +1,66 @@
+var images = new Array;
+images[1] = "https://images.unsplash.com/photo-1460500063983-994d4c27756c?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=27c2758e7f3aa5b8b3a4a1d1f1812310";
+images[2] = "https://images.unsplash.com/photo-1460378150801-e2c95cb65a50?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=1b5934b990c027763ff67c4115b6f32c";
+images[3] = "https://images.unsplash.com/photo-1442551382982-e59a4efb3c86?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=e131e5a1a12b40fca905f81eb90df89d";
+images[4] = "https://images.unsplash.com/photo-1440613905118-99b921706b5c?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=d46f4d7f29ff0d8d6c73c85d36605664";
+images[5] = "https://images.unsplash.com/photo-1461409971633-aa0e46732112?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=1a7a528b8ec9839cf9d17d04ec48422c";
+images[6] = "https://images.unsplash.com/photo-1459664018906-085c36f472af?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=0790a8686cb07ac8851066ef544f1082";
+images[7] = "https://images.unsplash.com/photo-1458724029936-2cc6ee38f5ef?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=1cf1a3e7ee550644d99b4b5bec53ede9";
+images[8] = "https://images.unsplash.com/photo-1457685373807-8c4d8be4c560?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=8d850b229e2be9e43d905d821ecb2b6e";
+images[9] = "https://images.unsplash.com/photo-1456930266018-fda42f7404a7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=a6945a53354d6325a2bca0215b6b2e14";
+images[10] = "https://images.unsplash.com/photo-1460400355256-e87506dcec4f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=f35997a68b394f08caf7d46cf2d27791";
+images[11] = "https://images.unsplash.com/photo-1416169607655-0c2b3ce2e1cc?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=63abc6240e85e1bfd1c45eb84ba717b8";
+images[12] = "https://images.unsplash.com/photo-1453230806017-56d81464b6c5?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=ef7efa065a1306618fef80e4294a189c";
+images[13] = "https://images.unsplash.com/photo-1453668069544-b8dbea7a0477?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=ac479c6bdac208162a40af860c12ef54";
+images[14] = "https://images.unsplash.com/photo-1457369804613-52c61a468e7d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=aff2bf56593d08c8fdda8de3eced4910";
+images[15] = "https://images.unsplash.com/photo-1440227537815-f4476b789291?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=056eb7e350ea8c020fba9ab343c38d26";
+images[16] = "https://images.unsplash.com/photo-1421749810611-438cc492b581?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=6cf5c5e54ebe49a76b78b7e17ca3dd47";
+images[17] = "https://images.unsplash.com/photo-1453106037972-08fbfe790762?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=cbaaa89f2c5394ff276ac2ccbfffd4a4";
+images[18] = "https://images.unsplash.com/photo-1452110040644-6751c0c95836?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=a441e2ab516fdcc2444eb8ddf65b923e";
+images[19] = "https://images.unsplash.com/photo-1433190152045-5a94184895da?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=4578a679069b9fb7be2d39d5d2d1e2bf";
+images[20] = "https://images.unsplash.com/uploads/14128434147336bfb286b/e76494ac?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=cedc00aca576c1b39b8e8c91d1967711";
+images[21] = "https://images.unsplash.com/photo-1454817481404-7e84c1b73b4a?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=5f4cef962c8ddea0da93e59299350831";
+images[22] = "https://images.unsplash.com/photo-1452723312111-3a7d0db0e024?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=774e99d16e3f3f0e4138be9d98e4f0f9";
+images[23] = "https://images.unsplash.com/photo-1449452198679-05c7fd30f416?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=73181f1c6d56b933b30de2bfe21fdf3b";
+images[24] = "https://images.unsplash.com/photo-1451188502541-13943edb6acb?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=892b76bb136c32b4d606b4a4d2f9b39b";
+images[25] = "https://images.unsplash.com/photo-1442473483905-95eb436675f1?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=8a25776c0f9340b38d567c53870da02f";
+images[26] = "https://images.unsplash.com/photo-1452800185063-6db5e12b8e2e?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=2700f9770386e4c5ebc53502a9099966";
+images[27] = "https://images.unsplash.com/photo-1447834353189-91c48abf20e1?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=aabe4d03f2c11c074d122c51d7925a78";
+images[28] = "https://images.unsplash.com/photo-1447069387593-a5de0862481e?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=0dab06e522cf4cd96ee75e9801e73c8a";
+images[29] = "https://images.unsplash.com/photo-1433769778268-24b797c4fc9a?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=f4a624999b2de6adcbde1f60526f6e8a";
+images[30] = "https://images.unsplash.com/reserve/Mj7V8XkOQLqVOyeXxixu_fierari-boardedwindows.jpg?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=8ead1af88eaabc50fc1a7c80d8a387a2";
+
+
+function getQuote() {
+  $("#quoteTitle").hide();
+  var imgCount = 30;
+  var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;
+  document.body.style.backgroundImage = 'url(' + images[randomCount] +
+    ')';
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundAttachment = "fixed";
+  document.body.style.backgroundPosition = "center center";
+
+  $.ajax({
+    headers: {
+      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+    }, //Where do you find all dis
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+    //where do you get this url from
+    success: function(response) {
+      var currentQuote = '',
+        currentAuthor = '';
+      var data = JSON.parse(response);
+      currentQuote = data.quote;
+      currentAuthor = data.author;
+      $("#quote").text('"' + currentQuote + '"');
+      $("#quoteSayer").text("-" + currentAuthor);
+    }
+  });
+}
+
+$(document).ready(function() {
+  $("#getMessage").click(getQuote);
+});
